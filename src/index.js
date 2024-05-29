25b929d7f1dbca834fb964d9cadab262474f8006
MbiamouRichy <richymbiamou52@gmail.com>
2024-05-29 12:33:42 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Page from "./page";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Page />);
+root.render(
+  <>
+    <Page />
+    <Analytics />
+  </>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
